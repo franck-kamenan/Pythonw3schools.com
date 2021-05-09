2d72500c754d1a38e9ce9a9bc477a70819c6f2af
Franck Kamenan <franck.kamenan@yahoo.com>
2021-05-09 16:00:34 +0200

    Data Types, Getting the Data Type, Setting the Data Type

---
+++ dataTypes.py
@@ -0,0 +1,42 @@
+a = "Hello World"                   # str
+print(type(a))
+print(a)
+b = 20                              # int
+print(type(b))
+print(b)
+c = 20.5                            # float
+print(type(c))
+print(c)
+d = 1j                              # complex
+print(type(d))
+print(d)
+e = ["apple", "banana", "cherry"]   # list
+print(type(e))
+print(e)
+f = ("apple", "banana", "cherry")   # tuple
+print(type(f))
+print(f)
+g = range(6)                        # range
+print(type(g))
+print(g)
+h = {"name" : "John", "age" : 36}   # dict
+print(type(h))
+print(h)
+i = {"apple", "banana", "cherry"}   # set
+print(type(i))
+print(i)
+j = frozenset({"apple", "banana"})  # frozenset
+print(type(j))
+print(j)
+k = True                            # bool
+print(type(k))
+print(k)
+l = b"Hello"                        # bytes
+print(type(l))
+print(l)
+m = bytearray(5)                    # bytearray
+print(type(m))
+print(m)
+n = memoryview(bytes(5))            # memoryview
+print(type(n))
+print(n)
