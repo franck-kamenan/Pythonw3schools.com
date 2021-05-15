2baee9b1bd6e18dce06f754dcbdccb81e5575ff5
Franck Kamenan <franck.kamenan@yahoo.com>
2021-05-15 10:43:28 +0200

    Math, The Math Module

---
+++ math.py
@@ -0,0 +1,25 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+print(x)
+
+# The Math Module
+import math
+x = math.sqrt(64)
+print(x)
+
+import math # Import math library
+x = math.ceil(1.4) # Round a number upward to its nearest integer
+y = math.floor(1.4) # Round a number downward to its nearest integer
+print(x)
+print(y)
+
+import math
+x = math.pi
+print(x)
